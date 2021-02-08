aa31fd84ecaa7b8782e706a20cff94cd76cdad81
rawshad <rawshad@outlook.com>
2021-02-08 22:21:23 +0600

    another commit

--- app.js
+++ app.js
@@ -9,9 +9,23 @@ const searchFood = () => {
 const displayFood = items => {
     const foodContainer = document.getElementById("food-container");
     items.forEach(meal => {
-        // console.log(meal.strMeal)
-        const li = document.createElement("li");
-        li.innerText = meal.strMeal;
-        foodContainer.appendChild(li);
+        console.log(meal);
+        const foodDiv = document.createElement("div");
+        foodDiv.className = 'card-body';
+        foodDiv.innerHTML = `<img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${meal.strMeal}</h5>
+                <p class="card-text">${meal.strArea}</p>
+                <ul>
+                    <li>${meal.strIngredient1}</li>
+                    <li>${meal.strIngredient2}</li>
+                    <li>${meal.strIngredient3}</li>
+                    <li>${meal.strIngredient4}</li>
+                    <li>${meal.strIngredient5}</li>
+                </ul>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>`
+        // li.innerText = meal.strMeal;
+        foodContainer.appendChild(foodDiv);
     });
 }
