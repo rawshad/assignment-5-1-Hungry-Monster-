27d095482030ac6b58ecda8ecd33e731fe9bc620
rawshad <rawshad@outlook.com>
2021-02-09 22:00:53 +0600

    Final Submission

--- app.js
+++ app.js
@@ -1,31 +1,55 @@
 const searchFood = () => {
-    const foodName = document.getElementById("searchFood").value;
-    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${foodName}`
+    const searchText = document.getElementById("search-food").value;
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
+    //load data
     fetch(url)
     .then(res => res.json())
     .then(data => displayFood(data.meals))
+    .catch(error => errorHandler('OOps! No Food Found.'))
 }
 
-const displayFood = items => {
-    const foodContainer = document.getElementById("food-container");
-    items.forEach(meal => {
-        console.log(meal);
-        const foodDiv = document.createElement("div");
-        foodDiv.className = 'card-body';
-        foodDiv.innerHTML = `<img src="${meal.strMealThumb}" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">${meal.strMeal}</h5>
-                <p class="card-text">${meal.strArea}</p>
-                <ul>
-                    <li>${meal.strIngredient1}</li>
-                    <li>${meal.strIngredient2}</li>
-                    <li>${meal.strIngredient3}</li>
-                    <li>${meal.strIngredient4}</li>
-                    <li>${meal.strIngredient5}</li>
-                </ul>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>`
-        // li.innerText = meal.strMeal;
+//After search show the food item
+const displayFood = foodItem => {
+    foodItem.forEach(items => {
+        const foodContainer = document.getElementById("food-container");
+        const foodDiv = document.createElement('div');
+        foodDiv.className = 'col-md-3';
+        foodDiv.innerHTML = `
+            <figure onclick="foodDetails('${items.strMealThumb}','${items.strMeal}', '${items.strIngredient1}', '${items.strIngredient2}', '${items.strIngredient3}', '${items.strIngredient4}', '${items.strIngredient5}', '${items.strIngredient6}')" class="figure">
+                <img src="${items.strMealThumb}" class="figure-img img-fluid rounded" alt="...">
+                <figcaption class="figure-caption text-center food-caption">${items.strMeal}</figcaption>
+            </figure>
+        `
         foodContainer.appendChild(foodDiv);
-    });
+    })
 }
+
+
+//Click on item then show details
+const foodDetails = (thumbnail, foodName, ing1, ing2, ing3, ing4, ing5, ing6) => {
+    // console.log(foodName, ing1, ing2, ing3, ing4, ing5, ing6);
+    const ingredientCard = document.getElementById("ingredients-card");
+    ingredientCard.innerHTML =`
+            <img src="${thumbnail}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${foodName}</h5>
+                <p class="card-text fw-bold">Ingredients</p>
+                <ul class="list-group">
+                <li class="list-group-item">${ing1}</li>
+                <li class="list-group-item">${ing2}</li>
+                <li class="list-group-item">${ing3}</li>
+                <li class="list-group-item">${ing4}</li>
+                <li class="list-group-item">${ing5}</li>
+                <li class="list-group-item">${ing6}</li>
+                </ul>
+            </div>
+    `
+}
+
+//Error handler
+const errorHandler = (error) => {
+    const errorMessage = document.getElementById("error-message");
+    errorMessage.innerText = error;
+}
+
+//Code writing based on ES6
